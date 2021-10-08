4d063bc2d7a7aeac85170d70f9e5aef8bf63e671
codersait <codersait@gmail.com>
2021-10-08 22:29:07 +0300

    update

--- notes/reverse-word.js
+++ notes/reverse-word.js
@@ -1,11 +1,19 @@
 // Reverse Word
-const word = 'clarusway'
-let reversedWord = ''
+const word = 'clarusway';
+let reversedWord = '';
 
 // First Way
 for (i = word.length - 1; i >= 0; i--) {
-  reversedWord += word.charAt(i)
+  reversedWord += word.charAt(i);
+  // reversedWord += word[i];
 }
 
+// console.log(reversedWord);
+
 // Second Way
-reversedWord = [...word].reverse().join('')
+
+reversedWord = [...word].reverse().join('');
+reversedWord = word.split('').reverse().join('');
+
+console.log(word.slice(1, 5));
+// console.log(word.split(''));
